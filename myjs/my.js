9c5d430ccdbeaa67c7bb56f336ca15628a44baec
sameesh-s <sameesh.fisat@gmail.com>
2016-03-04 14:18:08 +0530

    using JQuery ajax calls rather than links

---
+++ myjs/my.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+    $('#content').load('Github.html').fadeIn('slow');
+    })
+
+function github(){
+    $('#content').load('Github.html').fadeIn('slow');
+
+	}
+function contact(){
+    $('#content').load('Contact.html').fadeIn('slow');
+
+    }
+function resume(){
+    $('#content').load('Resume.html').fadeIn('slow');
+
+    }
